68ff08ba63eeab651b74d5955d9c196b202457af
Atolagbe David Tolulope <atolagbedt@gmail.com>
2022-11-09 00:11:27 +0100

    ui done

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <header>
         <h1>{{ title }}</h1>
-        <Button @toggle-add-task="$emit('toggle-add-task')" text="Add tasks" color="green"/>
+        <Button @btn-click="$emit('toggle-add-task')" :text="showAddTask ? 'Close' :'Add tasks'" :color="showAddTask ? 'red': 'green'"/>
     </header>
 </template>
 
