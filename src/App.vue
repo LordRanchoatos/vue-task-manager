0213225621d6aae81f3f4fbe6f21e2aa816ec1d7
Atolagbe David Tolulope <atolagbedt@gmail.com>
2022-11-09 08:13:05 +0100

    added proxy to vue.config.js

--- src/App.vue
+++ src/App.vue
@@ -48,7 +48,12 @@ export default {
       )
     },
     async fetchTasks() {
-      const res = await fetch('localhost:5000/tasks')
+      const res = await fetch('api/tasks')
+      const data = await res.json()
+      return data
+    },
+    async fetchTask(id) {
+      const res = await fetch(`api/tasks${id}`)
       const data = await res.json()
       return data
     }
